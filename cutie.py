492f6e46d7aa7d56a8ea4424a83606cb10735bba
izartorius <atreuseinzbelt@gmail.com>
2025-02-26 01:23:32 +0800

    added a function that evaluates expressions

--- cutie.py
+++ cutie.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 import math
+
+def calculate():
+    try:
+        expression = entry.get()
+        result = eval(expression), {"_builtins_":None},math._dict_)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END), str(result))
+    except Exception as e:
+        messagebox.showerror("Error", f"Invalid Expression: {e}")
+
